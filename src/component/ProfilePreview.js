598e61d10c71d623e033002297220be893274a96
Akash <akkygreen14@gmail.com>
2024-02-19 14:45:40 +0530

    Adding frontend code

--- src/component/ProfilePreview.js
+++ src/component/ProfilePreview.js
@@ -1,23 +1,50 @@
 import React from 'react';
+import { useNavigate } from 'react-router-dom';
 
 const ProfilePreview = ({ formData, setShowPreview }) => {
   const handleClose = () => {
-    setShowPreview(false);
+    window.location.reload()
+    // navigate("/get-profiles")
+    // setShowPreview(false);
   };
+  const navigate = useNavigate()
 
   return (
     <div>
       <h2>Profile Preview</h2>
-      <div>
-        <span onClick={handleClose} style={{ cursor: 'pointer' }}>
-          Close
-        </span>
-      </div>
-      <p>Name: {formData.name}</p>
-      <p>Age: {formData.age}</p>
+      <img src={`http://localhost:5000/${encodeURIComponent(formData.profilePicture)}`} alt="Profile" style={{ width: '200px', height: '200px' }} />
+      <p>First Name: {formData.firstName}</p>
+      <p>Last Name: {formData.lastName}</p>
+      <p>Email: {formData.email}</p>
+      <p>Mobile No.: {formData.mobile}</p>
+      <p>Date of Birth: {formData.birthDate}</p>
       <p>Gender: {formData.gender}</p>
+      <p>Height: {formData.height}</p>
+      <p>Diet: {formData.diet}</p>
+      <p>Living In: {formData.livingIn}</p>
+      <p>Language: {formData.language}</p>
+      <p>Religion: {formData.religion}</p>
+      <p>Marital status: {formData.maritalStatus}</p>
+      <p>Community: {formData.community}</p>
+      <p>Sub Community: {formData.subCommunity}</p>
       <p>Location: {formData.location}</p>
-      {/* Display other profile details */}
+      <p>Higest Qualification: {formData.highestQualification}</p>
+      <p>College Name: {formData.collegeName}</p>
+      <p>Work Type: {formData.workType}</p>
+      <p>Company Name: {formData.companyName}</p>
+      <p>Position: {formData.position}</p>
+      <p>Income: {formData.income}</p>
+      <p>Express Yourself: {formData.expressYourself}</p>
+      <p>Family Type: {formData.familyType}</p>
+      <p>Father's Occupation: {formData.fatherOccupation}</p>
+      <p>Mother's Occupation: {formData.motherOccupation}</p>
+      <p>Siblings: {formData.siblings}</p>
+      <p>Family Location: {formData.familyLocation}</p>
+      <p>Contact Address: {formData.contactAddress}</p>
+      <p>Family About: {formData.familyAbout}</p>
+      <div>
+      </div>
+      <button onClick={handleClose}>Go to Profiles</button>
     </div>
   );
 };
